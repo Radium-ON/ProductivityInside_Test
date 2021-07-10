9b7694dcaf8adc904f92b45b6b678db8e09beb92
Rodion <Radium-ON@outlook.com>
2021-07-10 18:59:43 +0700

    Добавил ресурсы акриловых кистей. Расширил макет до заголовка.

--- CurrencyConverter/Views/ConverterPage.xaml.cs
+++ CurrencyConverter/Views/ConverterPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using CurrencyConverter.ViewModels;
 
 using Windows.UI.Xaml.Controls;
@@ -14,6 +16,14 @@ namespace CurrencyConverter.Views
         public ConverterPage()
         {
             InitializeComponent();
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            if (titleBar != null)
+            {
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+                titleBar.ButtonForegroundColor = Colors.DarkSlateGray;
+            }
             DataContext = App.Current.Services.GetService<ConverterViewModel>();
         }
     }
